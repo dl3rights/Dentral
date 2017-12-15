2a86d4dd0fb0d66d54463739cf3a0bf2d40d87db
ratchakorn pattaranavik <l3right_z@hotmail.com>
2017-12-16 00:51:57 +0700

    Add project files.

---
+++ Dentral/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim MaxRe As Boolean = False
+    Dim point As New System.Drawing.Point()
+    Dim X, Y As Integer
+    Dim Default_Gray As Color
+    Dim User_ID As String
+    Dim User_name As String
+    Dim Priv_Class As String
+    Dim tble_ucp As New DataTable("Table")
+
+    Dim index As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+Public Class DblBufferedPanel
+    Inherits Panel
+    Public Sub New()
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+        Me.SetStyle(ControlStyles.ResizeRedraw, True)
+        Me.SetStyle(ControlStyles.UserPaint, True)
+        Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+    End Sub
+End Class
